2636b83046c549f09348d9d5ff81f4f00647c80f
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-25 21:30:34 -0600

    test: initial test for user story 1 created

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -25,4 +25,21 @@ RSpec.describe "User Registration" do
     expect(page).to have_content("Email has already been taken")
     
   end
+
+  describe "As a visitor when I visit `/register`" do
+    describe "I see a form to fill in my name, email, password, and password confirmation." do
+      it "When I fill in that form with my name, email, and matching passwords, I'm taken to my dashboard page `/users/:id`" do
+        visit register_path
+
+        fill_in :user_name, with: 'User Two'
+        fill_in :user_email, with:'notunique@example.com'
+        fill_in :password, with: 'Test'
+        fill_in :password_confirmation, with: 'Test'
+
+        click_button 'Create New User'
+
+        expect(current_path).to eq(user_path(User.last.id))
+      end
+    end
+  end 
 end
