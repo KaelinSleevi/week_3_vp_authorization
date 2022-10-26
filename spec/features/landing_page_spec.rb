a868cc4c7dfdd8fa08c89a9066fe4bbf61871f29
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-25 22:30:14 -0600

    test: add test written for user story 3

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Landing Page' do
     before :each do 
-        user1 = User.create(name: "User One", email: "user1@test.com", password: "Test")
-        user2 = User.create(name: "User Two", email: "user2@test.com", password: "Test")
+        @user1 = User.create(name: "User One", email: "user1@test.com", password: "Test")
+        @user2 = User.create(name: "User Two", email: "user2@test.com", password: "Test")
         visit '/'
     end 
 
@@ -33,5 +33,28 @@ RSpec.describe 'Landing Page' do
             expect(page).to have_content(user2.email)
         end     
 
-    end 
+    end
+
+    describe "As a registered user when I visit the landing page `/`" do
+        describe "I see a link for 'Log In' when I click on 'Log In'" do
+            describe "I'm taken to a Log In page ('/login') where I can input my unique email and password." do
+                it "when I enter my unique email and correct password, I'm taken to my dashboard page" do
+                    expect(page).to have_link("Log In")
+                    
+                    click_link "Log In"
+    
+                    expect(current_path).to eq(login_path)
+                
+                    fill_in 'Username', with: @user1.user_name
+                    fill_in 'Password', with: @user1.password
+                
+                    click_on "Submit"
+                
+                    expect(current_path).to eq(root_path)
+                
+                    expect(page).to have_content("Welcome, #{@user1.name}")
+                end
+            end
+        end
+    end
 end
